8f53a4b11def009a6223ef98ead22237b12799fb
flaw03 <100543813+flaw03@users.noreply.github.com>
2022-10-26 14:34:50 +0200

    création de la classe Humain

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,17 @@
+package histoire;
+
+import personnages.*;
+
+public class HistoireTP4 {
+	public static void main(String[] args) {
+		Humain yoann = new Humain("yohan", 10, "sprint");
+		yoann.direBonjour();
+		yoann.getArgent();
+		yoann.acheter(" un pc gaming", 3);
+		yoann.acheter("un coca cola cherry", 10 );
+		
+		
+		
+			
+	}
+}
---
+++ src/personnages/Humain.java
@@ -0,0 +1,60 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private int argent;
+	private String boisson;
+	
+	
+	
+	public Humain(String nom, int argent, String boisson) {
+		this.nom = nom;
+		this.argent = argent;
+		this.boisson = boisson;
+	}
+	
+
+	public int getArgent() {
+		return argent;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	public void parler(String texte) {
+		System.out.println( nom + " :" + texte);
+		
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour Je m’appelle " + nom + " et j’aime boire du " + boisson);
+	}
+	
+	public void boire(String boisson) {
+		parler("Mmmm, un bon verre de" + boisson + " ! GLOUPS !");
+	}
+	
+	public void acheter( String bien ,int prix) {
+		if (prix < argent) {
+			parler("J'ai assez d'argent pour m'acheter " + bien +" à " + prix + "e.");
+			perdreArgent(prix);
+		}
+		else {
+			parler("J'ai que " + argent + "je ne peux pas m'acheter " + bien + " à " + prix);
+		}
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain;
+	}
+	
+	public void perdreArgent(int perte) {
+		argent -= perte;
+	}
+	
+	
+	
+	
+}
